e89fbe047b751bc1c6917359f25fc09eb978804e
tonimota <toni.elton1993@gmail.com>
2018-07-09 21:50:03 -0300

    [FEAT] Update connection BD and include mongoose

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
+// Conexão
+mongoose.connect('mongodb://tonielton:toni123@ds233061.mlab.com:33061/node-store')
 // Rotas
 const index = require('./routes/index');
 const produtcs = require('./routes/products');
